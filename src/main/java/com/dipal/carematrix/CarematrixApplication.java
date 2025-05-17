9253b6d62592d3dbb7e812db830f4e0c7b20f1a7
Dipal Das <dasdipal8888@gmail.com>
2025-05-18 02:37:54 +0530

    first commit

---
+++ src/main/java/com/dipal/carematrix/CarematrixApplication.java
@@ -0,0 +1,13 @@
+package com.dipal.carematrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarematrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarematrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipal/carematrix/CarematrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipal.carematrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarematrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
